d6b4df1002ca37cec539e6b5fa764d1ec14d7242
lideo <1573567+lideo@users.noreply.github.com>
2018-09-01 12:21:47 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const devMode = process.env.NODE_ENV !== 'production';
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+
+module.exports = {
+    mode: devMode ? 'development' : 'production',
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: false }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new WebpackCleanupPlugin(),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+            hash: devMode ? false : true
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    }
+}
